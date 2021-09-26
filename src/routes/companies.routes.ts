0c80051d82b74835d3e669cdc587d8232994ee9e
Lucas Torres <lucastorresfellicio@gmail.com>
2021-09-26 00:38:26 -0300

    feat: config multer

--- src/routes/companies.routes.ts
+++ src/routes/companies.routes.ts
@@ -1,12 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCompanyController } from "../modules/Companies/useCases/createCompany";
 import { listCompaniesController } from "../modules/Companies/useCases/ListCompanies";
 
 const companiesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 companiesRoutes.post("/", (request, response) => {
   return createCompanyController.handle(request, response);
+});
+
+companiesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  
+  return response.send();
 });
 
 companiesRoutes.get("/", (request, response) => {
