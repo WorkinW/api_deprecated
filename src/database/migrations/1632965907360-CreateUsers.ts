2bad120bb13cea325cc8a29cd71b0f2c4403cd44
Lucas Torres <lucastorresfellicio@gmail.com>
2021-09-29 23:02:55 -0300

    feat: created model migrate the users

---
+++ src/database/migrations/1632965907360-CreateUsers.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1632965907360 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "company_id",
+            type: "varchar",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "username",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "cpf",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "isAdmin",
+            type: "boolean",
+            default: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
