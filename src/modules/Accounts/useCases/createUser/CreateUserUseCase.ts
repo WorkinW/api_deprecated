b9a7cc2fcfe199707a5f79edb1e5fa6b272f0b25
Lucas Torres <lucastorresfellicio@gmail.com>
2021-10-05 21:25:22 -0300

    feat: created avatar from user

--- src/modules/Accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/Accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,7 @@
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
-import { AppError } from "../../../../errors/AppError";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,7 +10,7 @@ class CreateUserUseCase {
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUsersRepository
-  ) {}
+  ) { }
 
   async execute({
     name,
