d9cd0d59acb8c669a260464296bc12a9e24a93b5
Lucas Torres <lucastorresfellicio@gmail.com>
2021-10-09 19:47:49 -0300

    fix: column id from users need is primary

--- src/shared/infra/typeorm/migrations/1632965907360-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1632965907360-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1632965907360 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "company_id",
