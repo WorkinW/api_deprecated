b78ab78de1a9ab485af543a3446f966d1851fb7f
Lucas Torres <lucastorresfellicio@gmail.com>
2021-10-10 16:21:35 -0300

    feat: alter connection for seeds

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,26 @@
+import { UsersRepository } from "@modules/Accounts/infra/typeorm/repositories/UsersRepository";
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "@shared/errors/AppError";
+
+export async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { id } = request.user;
+
+  const usersRepository = new UsersRepository();
+
+  const user = await usersRepository.findById(id);
+
+  if (!user) {
+    throw new AppError("You must be logged in");
+  }
+
+  if (!user.isAdmin) {
+    throw new AppError("You must be an admin");
+  }
+
+  return next();
+}
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,22 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
+// interface IOptions {
+//   host: string;
+// }
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "localhost"; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
-  });
-});
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "localhost"; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({
+//     ...options,
+//   });
+// });
+
+export default async (host = "localhost"): Promise<Connection> => {
+  const connection = await getConnectionOptions();
+  return createConnection(
+    Object.assign(connection, {
+      host, //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    })
+  );
+};
