84cc7b58df6ce7eb5317d2b4f5a466c72da7d6fc
Lucas Torres <lucastorresfellicio@gmail.com>
2021-10-29 09:22:55 -0300

    feat: other test for spot

--- src/modules/Spots/useCases/createSpot/CreateSpotUseCase.spec.ts
+++ src/modules/Spots/useCases/createSpot/CreateSpotUseCase.spec.ts
@@ -16,7 +16,7 @@ describe("Create Spot", () => {
     createSpotUseCase = new CreateSpotUseCase(spotsRepositoryInMemory);
   });
 
-  it("should be able to create a new spot", async () => {
+  it("should be able to create a new spot where time position is entry", async () => {
     const spot = await createSpotUseCase.execute({
       user_id: "2b9fe14b-e706-4f44-bf36-d24e680c0e0e",
       company_id: "dcd9fd26-257d-4543-b3e0-f8e1f9f34afa",
@@ -30,6 +30,23 @@ describe("Create Spot", () => {
     expect(spot).toHaveProperty("company_id");
     expect(spot).toHaveProperty("time_position");
     expect(spot.time_position).toBe("entry");
+    expect(spot).toBeDefined();
+  });
+
+  it("should be able to create a new spot where time position is exit", async () => {
+    const spot = await createSpotUseCase.execute({
+      user_id: "2b9fe14b-e706-4f44-bf36-d24e680c0e0e",
+      company_id: "dcd9fd26-257d-4543-b3e0-f8e1f9f34afa",
+      time_position: Position.Exit,
+    });
+
+    console.log(spot);
+
+    expect(spot).toHaveProperty("id");
+    expect(spot).toHaveProperty("user_id");
+    expect(spot).toHaveProperty("company_id");
+    expect(spot).toHaveProperty("time_position");
+    expect(spot.time_position).toBe("exit");
     expect(spot).toBeDefined();
   });
 });
